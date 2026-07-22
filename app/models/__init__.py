d71b91f5483085523d04100385bd280ca69b8af0
mayankahuja <mayankahuja785@gmail.com>
2026-07-22 23:46:30 +0530

    create group tables

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,6 @@
 from app.models.user import User
 from app.models.message import Message
-
-__all__ = ["User", "Message"]
+from app.models.group import Group
+from app.models.groupMember import GroupMember
+from app.models.groupMessage import GroupMessage
+__all__ = ["User", "Message", "Group", "GroupMember", "GroupMessage"]
